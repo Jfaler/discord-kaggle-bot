f496a1666431fa4b733bfc457f56346d5d5a57b3
Justin Faler <8336385+Jfaler@users.noreply.github.com>
2019-09-07 00:47:22 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# bot.py
 import os
 import sys
 import discord
@@ -30,9 +31,17 @@ bot = commands.Bot(command_prefix='!')
 # This function will list all current competitions
 @bot.command(name='comp', help='Responds with a list of competitions')
 async def competitions(comp):
-    competition = api.competitions_list()
-    response = competition
-    await comp.send(response)
+    now = dt.now()
+    now = now.astimezone(timezone('UTC'))
+    await comp.send(i18n.t('kaggle.hi', hour=now.hour))
+    competitions_list = api.competitions_list()
+    for competition in competitions_list:
+        if getattr(competition, 'awardsPoints') and not getattr(competition, 'submissionsDisabled'):
+            deadline = getattr(competition, 'deadline')
+            deadline = deadline.astimezone(timezone('UTC'))
+            diff = deadline - now
+            if diff.days > 0:
+                await comp.send('{}: {}'.format(i18n.t('kaggle.to_go', days=diff.days), getattr(competition, 'title')))
 
 # This function will list all datasets
 @bot.command(name='datasets', help='Responds with a list of datasets')
@@ -47,5 +56,13 @@ async def leaderboard(kernels):
     kernel = api.kernels_list()
     response = kernel
     await kernels.send(response)
+
+# This function will list all kernels
+@bot.command(name='lb', help='Responds with Leaderboard')
+async def files(ctx):
+    ctx = api.competitions_submissions_list()
+    response = random.choice(ctx)
+    await ctx.send(response)
+
 
 bot.run(token)
